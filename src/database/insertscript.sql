7fd1d2a8a15a6039103b9667277c1adc5485a9a6
guillermo cabana caceres <guillecabana@hotmail.com>
2020-04-01 15:45:17 -0500

    Update insertscript.sql

--- src/database/insertscript.sql
+++ src/database/insertscript.sql
@@ -1,4 +1,4 @@
-use DBSensor;
+use dbsensor;
 
 insert into tiposensores(nombre) values ('temperatura');
 insert into tiposensores(nombre) values ('movimiento');
@@ -27,32 +27,27 @@ insert into tiposensores(nombre) values ('wireless');
 insert into tiposensores(nombre) values ('ethernet');
 
 
-insert into sensor(nombre,valor,created) values('temp1',25,'2020-02-10 14:20:00');
-insert into sensor(nombre,valor,created) values('temp1',26,'2020-02-10 14:22:00');
-insert into sensor(nombre,valor,created) values('temp1',25,'2020-02-10 14:23:00');
-insert into sensor(nombre,valor,created) values('temp1',27,'2020-02-10 14:25:00');
-insert into sensor(nombre,valor,created) values('temp1',25,'2020-02-10 14:20:00');
-insert into sensor(nombre,valor,created) values('temp2',25,'2020-02-10 14:21:00');
-insert into sensor(nombre,valor,created) values('temp2',24,'2020-02-10 14:25:00');
-insert into sensor(nombre,valor,created) values('temp1',25,'2020-02-10 14:27:00');
-
-
 insert into tiposhields(nombre) values ('arduino');
 insert into tiposhields(nombre) values ('node MCU');
 insert into tiposhields(nombre) values ('esp8266');
 insert into tiposhields(nombre) values ('raspberry');
 
-insert into shields(nombre,tiposhieldid) values ('minipro', 1,'minipro.jpg','minipro.jpg');
-insert into shields(nombre,tiposhieldid) values ('nano', 1,'nano.jpg','nano.jpg');
-insert into shields(nombre,tiposhieldid) values ('uno', 1,'uno.jpg','uno.jpg');
-insert into shields(nombre,tiposhieldid) values ('leonardo', 1,'leonardo.jpg','leonardo.jpg');
-insert into shields(nombre,tiposhieldid) values ('mega', 1,'leonardo.jpg','leonardo.jpg');
-insert into shields(nombre,tiposhieldid) values ('esp32', 2,'esp32.jpg','esp32.jpg');
-insert into shields(nombre,tiposhieldid) values ('esp8266', 2 , 'esp8266.jpg','esp8266.jpg');
-insert into shields(nombre,tiposhieldid) values (' zero', 4 ,'zero.jpg','zero.jpg');
-insert into shields(nombre,tiposhieldid) values (' I', 4 ,'i.jpg','i.jpg');
-insert into shields(nombre,tiposhieldid) values (' II', 4,'ii.jpg','ii.jpg');
-insert into shields(nombre,tiposhieldid) values (' III', 4 ,'iii.jpg','iii.jpg');
-insert into shields(nombre,tiposhieldid) values (' IV', 4, 'iv.jpg','iv.jpg');
+insert into shields(nombre,tiposhieldId,imagen,pines) values ('minipro', 1,'minipro.jpg','minipro.jpg');
+insert into shields(nombre,tiposhieldid,imagen,pines) values ('nano', 1,'nano.jpg','nano.jpg');
+insert into shields(nombre,tiposhieldid,imagen,pines) values ('uno', 1,'uno.jpg','uno.jpg');
+insert into shields(nombre,tiposhieldid,imagen,pines) values ('leonardo', 1,'leonardo.jpg','leonardo.jpg');
+insert into shields(nombre,tiposhieldid,imagen,pines) values ('mega', 1,'leonardo.jpg','leonardo.jpg');
+insert into shields(nombre,tiposhieldid,imagen,pines) values ('esp32', 2,'esp32.jpg','esp32.jpg');
+insert into shields(nombre,tiposhieldid,imagen,pines) values ('esp8266', 2 , 'esp8266.jpg','esp8266.jpg');
+insert into shields(nombre,tiposhieldid,imagen,pines) values (' zero', 4 ,'zero.jpg','zero.jpg');
+insert into shields(nombre,tiposhieldid,imagen,pines) values (' I', 4 ,'i.jpg','i.jpg');
+insert into shields(nombre,tiposhieldid,imagen,pines) values (' II', 4,'ii.jpg','ii.jpg');
+insert into shields(nombre,tiposhieldid,imagen,pines) values (' III', 4 ,'iii.jpg','iii.jpg');
+insert into shields(nombre,tiposhieldid,imagen,pines) values (' IV', 4, 'iv.jpg','iv.jpg');
 
+insert into roles(nombre) values ('admin');
+insert into roles(nombre) values ('usuario');
+insert into roles(nombre) values ('invitado')
 
+insert into usuarios(nombre,apellidos,dni,password,rolid) values ('admin','admin','99999999',AES_ENCRYPT('123456789','bicholin'),1);
+insert into usuarios(nombre,apellidos,dni,password,rolid) values ('guillermo','cabana','10671906',AES_ENCRYPT('1234nix','bicholin'),2);
